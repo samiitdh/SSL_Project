9900a7fee2c8fe3c59a23cc340f3c9285357c6cc
samiitdh <190010024@iitdh.ac.in>
2020-12-06 16:29:33 +0530

    Modified POST to GET for roll number

--- add_individual_helper.php
+++ add_individual_helper.php
@@ -12,11 +12,26 @@ if($role == "student")
     $query = "SELECT `unique_id` FROM `individual` WHERE `roll_no` = '$roll_no' ;";
     $result = mysqli_query($con, $query);
     if($result->num_rows == 0)
-    {
+    {   
+        $check = true;
         $query = "INSERT INTO `individual` (`unique_id`, `name`, `roll_no`, `password`, `role`, `event_array`) VALUES (NULL, '$name', '$roll_no', '$password', 'student', '');";
         $bool = mysqli_query($con, $query);
-        if($bool)
-            echo "Inserted";
+        if(!$bool)
+            $check = false;
+        $table_name1 = $roll_no."_timetable";
+        $table_name2 = $roll_no."_events";
+        $sql1 = "CREATE TABLE `adiutor`.`$table_name1` ( `unique_id` INT NOT NULL AUTO_INCREMENT , `title` TEXT NOT NULL , `description` TEXT NOT NULL , `datetime` DATETIME NOT NULL , PRIMARY KEY (`unique_id`)) ENGINE = InnoDB; ;";
+        $bool = mysqli_query($con, $sql1);
+        if(!$bool)
+            $check = false;
+        $sql2 = "CREATE TABLE `adiutor`.`$table_name2` ( `unique_id` INT NOT NULL AUTO_INCREMENT , `title` TEXT NOT NULL , `description` TEXT NOT NULL , `datetime` DATETIME NOT NULL , PRIMARY KEY (`unique_id`)) ENGINE = InnoDB; ;";
+        $bool = mysqli_query($con, $sql2);
+        if(!$bool)
+            $check = false;
+        if($check)
+            echo "Inserted\n";
+        else
+            echo "Problem Insertion\n";
     }
     else
     {
