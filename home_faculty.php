aa97d89231524481c397779e840396a5ffdbb07f
SathvikGoud <74651495+SathvikGoud@users.noreply.github.com>
2020-12-06 10:28:58 +0530

    Add files via upload

---
+++ home_faculty.php
@@ -0,0 +1,5 @@
+<?php
+echo "<form action='view_events.php'><input type=submit value='Events'></form>";
+echo "<form action='change_pswd.php'><input type=submit value='Change Password'></form>";
+echo "<form action='faculty_room.php'><input type=submit value='View Room Allotment'></form>";
+?>
